6128efe303c96120e0516f068af2683d4cebdc50
Nathalie <nathalie.sophie.28@gmail.com>
2025-06-26 17:20:42 +0200

    update examples

--- examples/0b_manager_basics.py
+++ examples/0b_manager_basics.py
@@ -15,14 +15,20 @@ ext = '.sgy' # shot file extension
 settings = create_settings(fmin=10, fmax=50,                  # frequency range
                          vmin=10, vmax=1000, velstep=1)     # testing phase velocity range and step
 
+# %% 1. Create a new project
+# this will
+# -create the project directory structure if it does not exist
+# - copy raw data and geometry into the project directory if paths are provided
+
 # manager
-swam = BaseManager(f'{prj_dir}/proc/swa_v2/3b_MASW2D',
-                   path2raw=path2raw,
-                   path2geom=path2geom,
-                   settings=settings
+swam = BaseManager(f'{prj_dir}/proc/swa_v2/test', # project directory path
+                   path2raw=path2raw,                       # optional, copy & per default rename data from path (outside project directory)
+                   path2geom=path2geom,                     # optional, copy geom from path (outside project directory)
+                   settings=settings,                       # optional, define settings for visualisations and processing
+                   rename = False,                          # optional, rename copied raw data to preferred filename format, default value is False!
                    )
 
-# load data from database based on procset label
+# load data from database based on existing procset label
 swam.load_procset('raw')
 
 # set a new procset label
@@ -30,56 +36,48 @@ procset = 'test' # processing set label
 swam.set_procset_label(procset)
 
 # select data
-swam.select_data(sin=2, rep=1, inplace=True)
-
-# Other attributes for plotting of the stream data:
-# 'spectrogram','spectra','spectrogramComposite','FK','SFR','dispersionImage','dispersionImageComposite'
+swam.select_data(sin=1, rep=1, inplace=True)
 
 # preprocess the data
 # trim by offset
 processing_kwargs = {'min': 2, 'max': 10, 'which': 'reverse'} # arguments for the processing
 swam.preprocess(attr='trim',by = 'offset',**processing_kwargs)
 
+# remove zero amplitude data
+swam.preprocess(attr='check_traces')
+
+# plot selected data
 swam.plot('geometry')
 swam.plot('seismogram', amp_scale = 1, color = 'k')
-#
-# # processing_kwargs = {'manual': True,  'show':True}
-# swam.preprocess(attr='filter',by = 'mute', color = 'k', show_map = False, **processing_kwargs)
-#
-# swam.plot('seismogram', amp_scale = 1, color = 'k')
-
-# # remove zero amplitude data
-# swam.preprocess(attr='check_traces')
-#
-# #perform wavefield transformation based on the phaseshift method
-# swam.transform(method = 'phaseshift')
-
-# # %% Plotting
-# # plot the survey geometry
-# fig, ax = plt.subplots(figsize = (6,1))
-# swam.plot('geometry', axes = ax)
-# plt.savefig('/home/Natalie/Documents/Projects/GIT/swa/data/syn_data/figures_EGU/BaseManager/geom.png')
-#
-# # plot the seismogram
-# fig, ax = plt.subplots(figsize = (6,2))
-# swam.plot('seismogram',show_map = False, amp_scale = 1, color = 'k',axes = ax)
-# plt.savefig('/home/Natalie/Documents/Projects/GIT/swa/data/syn_data/figures_EGU/BaseManager/seismo.png')
-#
-# fig, ax = plt.subplots(figsize = (6,2))
-# swam.plot('dispersionImage', cmap = 'Greys',axes = ax)
-# plt.savefig('/home/Natalie/Documents/Projects/GIT/swa/data/syn_data/figures_EGU/BaseManager/disp.png')
-
+swam.plot('FK')
+swam.plot('spectra')
+swam.plot('spectrogram')
+swam.plot('SFR')
 
 # perform wavefield transformation based on the phaseshift method
-# swam.transform(method = 'fdbf')
-# swam.transform(method = 'phaseshift')
-#
-# swam.extract(pck_mode='manual', method = 'fdbf', cmap = 'Greys')
-#
-# # plot the dispersion images
-# fig,ax = plt.subplots(1,2,figsize=(10,5))
-# swam.plot('dispersionImage', method = 'phaseshift', axes = ax[0],cmap = 'Greys')
-# swam.plot('dispersionImage', method = 'fdbf', axes = ax[1],cmap = 'Greys')
-#
-# plt.tight_layout()
-# plt.show()
+swam.transform(method = 'fdbf')
+swam.plot('FV')
+
+# %% VIEWER
+# figure viewer
+swam.gui_view()
+swam.gui_view('FK')
+
+# interactive figure viewer
+swam.gui_interact()
+swam.gui_interact('FV')
+
+# plot picked dispersion curve
+swam.plot_curve(method = 'fdbf')
+
+# automatic dispersion curve extraction using MOPA
+swam.extract(pck_mode='auto', method = 'MOPA')
+swam.plot_curve(method = 'MOPA')
+
+# automatic dispersion curve extraction using trafo and max
+# (INFO:
+# I changed the use of method here, to make it less confusing hopefully.
+# method now only refers to the auto extraction method
+# you don't have to combine the trafo method and the extraction method)
+swam.extract(pck_mode='auto', method = 'max') # method here can be 'max' or the trafo method
+swam.plot_curve(method = 'max')
