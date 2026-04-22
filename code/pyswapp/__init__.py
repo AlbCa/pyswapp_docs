8d7bede3b8a63ef78dd2b14dc3e00d99c660ad25
Alberto Carrera <96575367+AlbCa@users.noreply.github.com>
2026-04-22 15:12:59 +0200

    deploy docs

--- code/pyswapp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .manager import BaseManager, MASW2DManager, Tomo2DManager
-from .stream import SeismicStream
-from .curve import DispersionCurve
-from .curves import CombineCurves
-from .qtapps import (QApplication, DataSwitcherBase,DataSwitcherPick,DataSwitcherFilterTX,
-                     DataSwitcherFilterFK,DualDataSwitcher,FigureSwitcher)
-
-from .utils import *
-
